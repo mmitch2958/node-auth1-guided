b5607412fc53820521ee2b8f93bceb3a51d6081b
mmitch2958 <mikescottmitchell@outlook.com>
2020-10-12 14:33:00 -0400

    working

--- database/seeds/002-users.js
+++ database/seeds/002-users.js
@@ -1,13 +1,7 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function (knex) {
+  return knex("users").insert([
+      { username: "", password: "" },
+      { username: "", password: "" },
+      { username: "", password: "" },
+  ]);
 };
--- users/users-router.js
+++ users/users-router.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 
 router.get("/", (req, res) => {
-  Users.find()
-    .then(users => {
-      res.status(200).json(users);
-    })
-    .catch(err => res.send(err));
+    Users.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => res.send(err.message));
 });
 
 module.exports = router;
